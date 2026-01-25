44234ec46789e6598b67e925f0a32f5b75b65b53
hitech <113853895+hitechboi@users.noreply.github.com>
2026-01-25 16:17:07 -0600

    Update massarce.lua

--- massarce.lua
+++ massarce.lua
@@ -4,25 +4,4 @@ getgenv().NoRecoilEnabled = false
 getgenv().RecoilStrength = 10
 getgenv().RecoilKey = "F"
 
-local window = UI:Window({
-    Title = "Massacre",
-    Size = Vector2.new(350, 260)
-})
-
-local tweaks = window:Tab("Tweaks")
-local binds = window:Tab("Keybinds")
-
--- Checkbox
-UI:Checkbox(tweaks, "No Recoil", false, function(state)
-    getgenv().NoRecoilEnabled = state
-end)
-
--- Slider
-UI:Slider(tweaks, "Recoil Strength", 0, 100, 10, function(val)
-    getgenv().RecoilStrength = val
-end)
-
--- Keybind
-UI:Keybind(binds, "Recoil Toggle", "F", function(key)
-    getgenv().RecoilKey = key
-end)
+local window = UI:Window
