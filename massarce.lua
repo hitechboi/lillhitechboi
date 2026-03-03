ff38b26debe97605dfb9afcd1a6027de0a7ed4bb
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-03 17:23:02 -0600

    Delete massarce.lua

--- massarce.lua
+++
@@ -1,7 +0,0 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/hitechboi/lillhitechboi/main/ui.lua"))()
-
-getgenv().NoRecoilEnabled = false
-getgenv().RecoilStrength = 10
-getgenv().RecoilKey = "F"
-
-local window = UI:Window
